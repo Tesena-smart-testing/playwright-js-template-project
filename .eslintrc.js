e92f33b26568bc11ecf12b2df00f06c26b42ab6c
bednaJedna <bednarik.radek@gmail.com>
2020-08-07 07:59:52 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "babel-eslint",
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {},
+};
